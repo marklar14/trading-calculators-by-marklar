7f02f6a96babb8fa41fc5923a554bb827a17b536
Karel Trnka <marklar2010@gmail.com>
2026-05-12 00:39:10 +0200

    fix: missing module

--- src/app/core/layout/app-shell/app-shell.ts
+++ src/app/core/layout/app-shell/app-shell.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -7,7 +7,7 @@ import { Menu } from '../../../app/core/menu/menu';
 
 @Component({
   selector: 'app-app-shell',
-  imports: [RouterLinkActive, TranslocoPipe, ButtonModule, ToolbarModule, Menu],
+  imports: [RouterOutlet, TranslocoPipe, ButtonModule, ToolbarModule, Menu],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss',
 })
